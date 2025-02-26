1e430eb3fefcac868fd4d3008f8166bcb48ebe33
Victor-Queiroz-Mendes <victor.h.mendes7@aluno.senai.br>
2025-02-26 15:26:12 -0300

    Atualizando
    .

---
+++ SENAI/FRONT/DOM/script.js
@@ -0,0 +1,14 @@
+// let descricao = document.getElementsByClassName('descricao');
+
+// console.log(descricao);
+
+//let paragrafo = document.getElementsByTagName('button')
+// console.log(paragrafo); 
+
+// let element = document.querySelector('#titulo')
+// console.log(element); 
+//  let titulo = document.getElementById('titulo')
+//  titulo.innerHTML = "Texto alterado! <span style='color:red;'>Agora com uma span!</spam>"
+ 
+// let titulo = document.getElementById('titulo')
+// titulo.outerHTML = "<p id='titulo'>Titulo alterado</p>";
